3c1cd9dfaf374f588bd800172060f5d1e2130196
Katherine <ocheretianfws@gmail.com>
2021-03-23 03:36:04 +0200

    2.3

--- 2.2.py
+++ 2.2.py
@@ -2,13 +2,67 @@ from numpy import exp, array, random, dot, tanh
 import numpy as np
 
 
-input_values = np.array([8.5, 0.65, 1.2])
-print(input_values)
+input_values = np.array([[8.5, 0.65, 1.2],
+                         [9.5, 0.8, 1.3],
+                         [9.9, 0.8, 0.5],
+                         [9.0, 0.9, 1.0]])
 
-weight_matrix = 2 * random.random((3, 1)) - 1
-print(weight_matrix)
+weight_matrix = np.array([0.1, 0.2, -0.1])
+
+expected_output = np.array([1., 1., 0., 1.]).T
+
+alpha = 0.01
 
 
-output_values = np.dot(input_values, weight_matrix)
-print(output_values)
 
+# for i in range(1000):
+#     print(i+1)
+#     output_value = np.dot(input_values, weight_matrix.T)
+#     delta = output_value - expected_output
+#     weight_delta = np.dot(delta, input_values)
+#     weight_matrix = weight_matrix - np.dot(alpha, weight_delta)
+#
+#     # print("delta " + str(delta))
+#     # print("weight_delta " + str(weight_delta))
+#     # print("weight " + str(weight_matrix))
+#     print("output " + str(output_value))
+#     error = (output_value - expected_output) ** 2
+#     print("error " + str(error))
+#     print()
+
+er = 0
+
+for i in range(1000):
+    print(i+1)
+    output_value = np.dot(input_values[0], weight_matrix.T)
+    delta = output_value - expected_output[0]
+    weight_delta = np.dot(delta, input_values[0])
+    weight_matrix = weight_matrix - np.dot(alpha, weight_delta)
+
+    # print("delta " + str(delta))
+    # print("weight_delta " + str(weight_delta))
+    # print("weight " + str(weight_matrix))
+    print("output " + str(output_value))
+    error = (output_value - expected_output[0]) ** 2
+    print("error " + str(error))
+    er = er + error
+    print()
+
+print(er)
+
+
+# for i in range(1000):
+#     for ind in range(4):
+#         print(i+1)
+#         output_value = np.dot(input_values[ind], weight_matrix)
+#         delta = output_value - expected_output[ind]
+#         weight_delta = np.dot(delta, input_values[ind])
+#         weight_matrix = weight_matrix - np.dot(alpha, weight_delta)
+#
+#         print("delta " + str(delta))
+#         print("weight_delta " + str(weight_delta))
+#         print("weight " + str(weight_matrix))
+#         print("output " + str(output_value))
+#         error = (output_value - expected_output[ind]) ** 2
+#         print("error " + str(error))
+#         print()
---
+++ 2.3.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+input_values = np.array([[8.5, 0.65, 1.2],
+                         [9.5, 0.8, 1.3],
+                         [9.9, 0.8, 0.5],
+                         [9.0, 0.9, 1.0]])
+
+
+weight_matrix = np.array([[0.1, 0.1, -0.3],
+                          [0.1, 0.2, 0.0],
+                          [0.0, 1.3, 0.1]])
+
+expected_output = np.array([[0.1, 1, 0.1],
+                           [0, 1, 0],
+                           [0, 0, 0.1],
+                           [0.1, 1, 0.2]])
+
+alpha = 0.01
+
+
+
+for i in range(1000):
+    print(i+1)
+    output_value = np.dot(input_values[0], weight_matrix.T)
+    delta = output_value - expected_output[0]
+    weight_delta = delta * input_values[0]
+    print(weight_delta)
+    weight_matrix = weight_matrix - np.dot(alpha, weight_delta)
+
+    # print("delta " + str(delta))
+    # print("weight_delta " + str(weight_delta))
+    # print("weight " + str(weight_matrix))
+    print("output " + str(output_value))
+    error = (output_value - expected_output[0]) ** 2
+    print("error " + str(error))
+    print()
+
